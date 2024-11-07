391621a234ee99af7bc469decfec93aeeb5a3a1a
Joel <jmacdonald@membersolutions.com>
2024-11-07 20:39:54 -0300

    new logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily:{
-        sans: ['Poppins', 'san-serif']
-      },
-      gridTemplateColumns: {
-        '70/30': '70% 28%'
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Poppins", "san-serif"],
+				mono: ["Mona-Sans", "sans-serif"],
+			},
+			gridTemplateColumns: {
+				"70/30": "70% 28%",
+			},
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
