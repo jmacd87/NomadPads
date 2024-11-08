f97730dd306886f6f29f5f1eb548e5d8e34254d2
jmacd87 <jmacd87@gmail.com>
2024-11-08 12:06:31 -0300

    loader styling

--- src/app/components/PropertyCard.tsx
+++ src/app/components/PropertyCard.tsx
@@ -38,7 +38,7 @@ const PropertyCard: React.FC<PropertyCardProps> = ({ property }) => {
 						<div className="text-gray-600">{property.type}</div>
 						<h3 className="text-lg font-bold">{property.name}</h3>
 					</div>
-					<h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right text-lg">
+					<h3 className="absolute top-[10px] right-[10px] bg-white px-2 py-2 rounded-lg text-sm text-blue-500 font-bold text-right md:text-center lg:text-right text-lg">
 						{getRatesDisplay()}
 					</h3>
 
