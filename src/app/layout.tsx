13ec51b3953eccc817af69641e1171f36ab2cc4f
jmacd87 <jmacd87@gmail.com>
2024-11-08 20:04:28 -0300

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,33 +4,51 @@ import Footer from "./components/Footer"
 import AuthProvider from "./components/AuthProvider"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import { Metadata } from "next"
+import Script from "next/script"
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: "Buenos Aires Rentals",
 	keywords: "rental, property, buenos Aires, real estate",
 	description: "find the perfect long term rental property",
 }
+
 interface Props {
 	children: React.ReactNode
 }
 
+const GA_TRACKING_ID = "G-62WC2FS3TV" // Your Google Analytics ID
+
 const Layout: React.FC<Props> = ({ children }) => {
 	return (
 		<AuthProvider>
-			<html>
-				<head>
-					<link
-						href="https://fonts.cdnfonts.com/css/mona-sans"
-						rel="stylesheet"
+			{/* Google Analytics Script */}
+			{process.env.NODE_ENV === "production" && (
+				<>
+					<Script
+						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+						strategy="afterInteractive"
 					/>
-				</head>
-				<body className={"min-h-screen flex flex-col"}>
-					<Navbar />
-					<main>{children}</main>
-					<Footer />
-					<ToastContainer />
-				</body>
-			</html>
+					<Script id="google-analytics" strategy="afterInteractive">
+						{`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}');
+            `}
+					</Script>
+				</>
+			)}
+
+			{/* Custom font */}
+			<link href="https://fonts.cdnfonts.com/css/mona-sans" rel="stylesheet" />
+
+			<div className="min-h-screen flex flex-col">
+				<Navbar />
+				<main className="flex-grow">{children}</main>
+				<Footer />
+				<ToastContainer />
+			</div>
 		</AuthProvider>
 	)
 }
