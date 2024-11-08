e0833f2cc26f93f632c4c73f07c297ee775d4f11
jmacd87 <jmacd87@gmail.com>
2024-11-08 20:21:57 -0300

    remove GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import AuthProvider from "./components/AuthProvider"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import { Metadata } from "next"
-import Script from "next/script"
 
 export const metadata: Metadata = {
 	title: "Buenos Aires Rentals",
@@ -17,30 +16,9 @@ interface Props {
 	children: React.ReactNode
 }
 
-const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
-
 const Layout: React.FC<Props> = ({ children }) => {
 	return (
 		<AuthProvider>
-			{/* Google Analytics Script */}
-			{process.env.NODE_ENV === "production" && (
-				<>
-					<Script
-						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-						strategy="afterInteractive"
-					/>
-					<Script id="google-analytics" strategy="afterInteractive">
-						{`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}');
-            `}
-					</Script>
-				</>
-			)}
-
-			{/* Custom font */}
 			<link href="https://fonts.cdnfonts.com/css/mona-sans" rel="stylesheet" />
 
 			<div className="min-h-screen flex flex-col">
